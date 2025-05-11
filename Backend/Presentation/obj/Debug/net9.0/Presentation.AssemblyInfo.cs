834d05d966b6fd2afc7a04d1c3572b224ea68a84
oscar <oscar.w.ergun@gmail.com>
2025-05-11 18:35:43 +0200

    created login form

--- Backend/Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
+++ Backend/Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+302df6fe20e538c4de07f8ddb7e7188abc4a674b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ec1f93998a4bcc96e5fd072ddc516b5a33de874")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
