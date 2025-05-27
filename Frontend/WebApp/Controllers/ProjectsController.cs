948b49b71dd60d7b40c8072dbf04c4233561d59f
oscar <oscar.w.ergun@gmail.com>
2025-05-27 23:06:30 +0200

    create partials to optimize code

--- Frontend/WebApp/Controllers/ProjectsController.cs
+++ Frontend/WebApp/Controllers/ProjectsController.cs
@@ -6,7 +6,7 @@ namespace WebApp.Controllers
 {
     public class ProjectsController : Controller
     {
-
+        [Route("admin/projects")]
         public IActionResult Index()
         {
             return View();
--- Frontend/WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ Frontend/WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+778805b5260b9a3573bd9e1084794f637f04bba1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a45023f41d7a56db44da1e675235a5283bddd90")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
